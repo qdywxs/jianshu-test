cec51441a0c1e4217da4177afadc8216e890da35
oli-zhao <381942195@qq.com>
2019-08-09 11:21:15 +0800

    react_16

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 
-import PropTypes from "prop-types";
-
 class TodoItem extends Component {
   constructor(props) {
     super(props);
@@ -10,48 +8,31 @@ class TodoItem extends Component {
   } 
   
   render() {
-    console.log("TodoItem render")  /*2️⃣-③：在页面 input 框输入内容并点击“提交”后，
-                                    子组件的 render 函数都会相应执行。
-                               ❗️❗️❗️其原因有二：
-                               第一，子组件的 content 是从父组件接收的，在父组件 TodoList 里，
-                               content 的值是“列表循环出的每一项”。故，当我们输入内容并提交后，
-                               “列表循环出的每一项”会随之变化，继而子组件接收到的属性也会变化，
-                               子组件的 render 函数相应地会根据新的值进行重新渲染；
-                                      
-                               第二，当父组件的 render 函数被运行时，它的子组件的 render 函数
-                               都将被运行一次！
-                               因为，子组件 TodoItem 是被父组件 TodoList 的 render 渲染出来的，
-                               那么，父组件的 render 在被重新执行时，子组件的 render 也会被执行。
-                               */
-    
-    const {content, test} = this.props  /*2️⃣-①：this.props 是父组件通过“属性”传递过来的内容；*/
+    const {content} = this.props
     
     return(
       <div onClick={this.handleClick}> 
-      {test}{content}  {/*2️⃣-②：我们将内容项 content 展示在页面；*/}
+        {content}  
       </div>
     )
   }
   
   handleClick() {  
-    const {itemDelete, index} = this.props  
+    const {itemDelete, index} = this.props
     
     itemDelete(index)
   }
-}
 
-TodoItem.propTypes = {
-  
-  test: PropTypes.string.isRequired, 
-  
-  content: PropTypes.string,  
-  itemDelete: PropTypes.func,
-  index: PropTypes.number
-}
+  componentWillReceiveProps() { 
+    console.log("child componentWillReciveProps")
+  }
 
-TodoItem.defaultProps = {
+  componentWillUnmount() {  /*4️⃣这个“生命周期函数”是在“组件”
+                            即将被程序从页面上剔除时，会自动执行。*/
   
-  test: "hello, " 
+    console.log("child componentWillUnmount")
+  }
+
 }
 
 export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -7,7 +7,7 @@ import "./style.css";
 class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       inputValue: "", 
       list: []
     };
@@ -18,8 +18,14 @@ class TodoList extends Component {
     
   }
 
-  render() { 
-    console.log("render") 
+  componentWillMount() {
+    
+    console.log("componentWillMount") 
+  }
+  
+  render() {  /*3️⃣-③：“生命周期函数”render 会执行，根据变化了的“数据”，
+              重新“更新”页面；*/
+    console.log("parent render")
     
     return(
       <Fragment>
@@ -46,14 +52,39 @@ class TodoList extends Component {
       </Fragment>
     )
   }
+  
+  componentDidMount() {  
+  
+    console.log("componentDidMount")  
+  }
+
+  shouldComponentUpdate() {  /*3️⃣-①：这个“生命周期函数”是在“组件”
+                             被“更新”之前，会被自动执行；*/
+    
+    console.log("shouldComponentUpdate")
+    return true
+    
+  }
+
+  componentWillUpdate() {  /*3️⃣-②：这个“生命周期函数”是在“组件”
+                           被“更新”之前，shouldComponentUpdate 询问之后，
+                           且得到 true 的“答复”后，才自动执行；*/
+  
+    console.log("componentWillUpdate")
+  }
+
+  componentDidUpdate() {  /*3️⃣-④：这个“生命周期函数”是在“组件”
+                          被“更新”完成之后，会自动执行。*/
+    
+    
+    console.log("componentDidUpdate")
+  }
 
   getTodoItem() {
     return this.state.list.map((item, index) => { 
       return( 
-        
-        /*✅4️⃣用 item 作为 key 值；*/
         <TodoItem 
-        key={item}
+        key={index}
         
         content={item}
         index={index} 
@@ -73,7 +104,7 @@ class TodoList extends Component {
 
   handleBtnClick() {
     this.setState((prevState) => ({
-      list: [...prevState.list, prevState.inputValue],
+      list: [...prevState.list, prevState.inputValue],  
       inputValue: ""        
     }))
   }
