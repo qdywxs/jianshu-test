2228fc4cfba21d69a9b83926205dc6f4477e8c31
oli-zhao <381942195@qq.com>
2019-08-09 12:48:30 +0800

    react_25

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,15 @@
 import React, {Component} from "react";
 import 'antd/dist/antd.css';
 
+/*3️⃣-②：把 Antd Design 关于“页面渲染”的内容剪切；
 import { Input, Button, List } from 'antd';  
+*/
 
 import store from "./store";
-
-/*3️⃣-①：首先，将创建好的“方法”引入；*/
 import {getInputChangeAction, getAddItemAction, getDeleteItemAction} from "./store/actionCreators"; 
 
-/*3️⃣-⑧：以上几步完成后，下边这行代码在这里就没用了，故删除。*/
-/*import {CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM} from "./store/actionTypes"; */
+import TodoListUI from "./TodoListUI"; 
+
 
 class TodoList extends Component {
   constructor(props) {
@@ -23,46 +23,31 @@ class TodoList extends Component {
     
     this.handleButtonClick = this.handleButtonClick.bind(this); 
     
+    /*3️⃣-⑱：在这里对 handleItemDelete 方法的 this 指向作一个修改*/
+    this.handleItemDelete = this.handleItemDelete.bind(this);
+    
     store.subscribe(this.handleStoreChange);  
     
   }
   
   render() {
-    return (
-      <div style={{marginTop: "10px", marginLeft: "10px"}}>
-        <div>
-          <Input 
-            value={this.state.inputValue} 
-            placeholder="todo info" 
-            style={{width: "300px", marginRight: "10px"}} 
-            
-            onChange={this.handleInputChange}  
-          /> 
-          
-          <Button type="primary" onClick={this.handleButtonClick}>提交</Button>  
-
-          <List style={{marginTop: "10px", width: "300px"}} 
-
-            bordered
-            dataSource={this.state.list}
-            renderItem={(item, index) => <List.Item onClick = {this.handleItemDelete.bind(this, index)}>{item}</List.Item>}
-          />  
-          
-        </div>
-      </div>
+    return(
+      
+      /*3️⃣-⑤、3️⃣-⑧、3️⃣-⑪、3️⃣-⑭、3️⃣-⑰：
+      通过“属性”的形式传值给 TodoListUI 组件；*/
+      <TodoListUI
+        inputValue={this.state.inputValue}
+        list={this.state.list}  
+        handleInputChange={this.handleInputChange}
+        handleButtonClick={this.handleButtonClick}
+        handleItemDelete={this.handleItemDelete}
+      />
     )
   }
   
   
   handleInputChange(e) { 
-    /*3️⃣-②：将以下几行代码删除；
-    const action = {
-      type: CHANGE_INPUT_VALUE, 
-      
-      value: e.target.value  
-    }
-    */
-    /*3️⃣-③：直接调用 getInputChangeAction 方法：*/
+
     const action = getInputChangeAction(e.target.value)
     
     store.dispatch(action);  
@@ -75,25 +60,14 @@ class TodoList extends Component {
 
 
   handleButtonClick() { 
-    /*3️⃣-④：将以下几行代码删除；
-    const action = {  
-      type: ADD_TODO_ITEM 
-    };
-    */
-    /*3️⃣-⑤：直接调用 getAddItemAction 方法；*/
+    
     const action = getAddItemAction();  
     
     store.dispatch(action); 
   }
 
   handleItemDelete(index) { 
-    /*3️⃣-⑥：将以下几行代码删除；
-    const action = {
-      type: DELETE_TODO_ITEM,  
-      index 
-    };
-    */
-    /*3️⃣-⑦：直接调用 getDeleteItemAction 方法；*/
+
     const action = getDeleteItemAction(index);
     
     store.dispatch(action); 
