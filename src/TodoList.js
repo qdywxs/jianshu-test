c0e807af429ee71550d9fdb31311c40464f68505
oli-zhao <381942195@qq.com>
2019-08-09 11:40:07 +0800

    react_20

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,104 +1,47 @@
-import React, { Component, Fragment } from "react"; 
+import React, {Component} from "react";
+import 'antd/dist/antd.css';
 
-import TodoItem from "./TodoItem"; 
+import { Input, Button, List } from 'antd';  /*7️⃣-①：首先引入 List 组件；*/  
 
-import axios from "axios";  /*🚀引入 axios 模块。*/
+/*7️⃣-②：按官网写法，去声明 data 常量（即，要显示在列表项中的内容）；*/
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
 
-import "./style.css"; 
 
 class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: "", 
-      list: []
-    };
-    
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleBtnClick = this.handleBtnClick.bind(this);
-    this.handleItemDelete = this.handleItemDelete.bind(this);
-    
-  }
-
+  
   render() {
-    return(
-      <Fragment>
+    return (
+      <div style={{marginTop: "10px", marginLeft: "10px"}}>
         <div>
-          <label htmlFor="insertArea">请输入要进行的事项：</label>    
-        
-          <input 
-            id="insertArea"
-      
-            className="input"
-            value={this.state.inputValue}
-            onChange={this.handleInputChange}
+          <Input placeholder="todo info" style={{width: "300px", marginRight: "10px"}} /> 
+          
+          <Button type="primary">提交</Button>  
+
+          {/*7️⃣-③：直接复制官网写法（把没用的去掉），使用 List 组件：*/}
+          {/*7️⃣-④：给 List 加点样式（间距、宽度），使其更好看些。*/}
+          <List style={{marginTop: "10px", width: "300px"}} 
+
+            bordered
+            dataSource={data}
+            renderItem={item => <List.Item>{item}</List.Item>}
           />
+          {/*❗️❗️❗️
+            borderd 表示：这个列表有一个“边框”；
+            dataSource 表示：这个列表到底要渲染什么内容（答：上边声明的 data 里的数据）；
+            renderItem 表示：如何渲染（答：会将每条数据渲染成 List.item 这样的小组件，
+                       数据内容就是 data 里边每一项数据的内容）。
+          */}    
+          
 
-          <button onClick={this.handleBtnClick}>
-            提交
-          </button>
         </div>
-
-        <ul>
-          {this.getTodoItem()}
-        </ul>
-
-      </Fragment>
+      </div>
     )
-  }
-
-  componentDidMount() { 
-    axios.get("http://yapi.demo.qunar.com/mock/82169/api/todolist")  
-    
-      .then((res) => {
-        
-        this.setState(() => ({  /*4️⃣-④：调用 setState 方法，传入一个“箭头函数”，
-                                让“数据项”list 的值为 res.data。*/
-          list: [...res.data]
-        }))
-      
-      })
-    
-      .catch(() => {alert("error")})  
-  }
-  
-  getTodoItem() {
-    return this.state.list.map((item, index) => { 
-      return( 
-        <TodoItem 
-        key={index}
-        
-        content={item}
-        index={index} 
-        itemDelete={this.handleItemDelete}
-        />  
-      )  
-    })
-  }
-  
-  handleInputChange(e) {
-    const value = e.target.value
-    
-    this.setState(() => ({
-      inputValue: value
-    }))
-  }
-
-  handleBtnClick() {
-    this.setState((prevState) => ({
-      list: [...prevState.list, prevState.inputValue],  
-      inputValue: ""        
-    }))
-  }
-
-  handleItemDelete(index) { 
-    this.setState((prevState) => {
-      const list = [...prevState.list]
-      list.splice(index, 1)
-        
-      return {list}
- 
-    })
   }
 }
 
