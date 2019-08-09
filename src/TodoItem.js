2843f33352aeabad5e8205c57d95ecab013534b2
oli-zhao <381942195@qq.com>
2019-08-09 10:22:42 +0800

    react_11

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import PropTypes from "prop-types";
+
 class TodoItem extends Component {
   constructor(props) {
     super(props);
@@ -8,20 +10,37 @@ class TodoItem extends Component {
   } 
   
   render() {
-    const {content} = this.props
+    const {content, test} = this.props 
     
     return(
       <div onClick={this.handleClick}> 
-        {content}  
+      {test}{content}  
       </div>
     )
   }
   
   handleClick() {  
-    const {itemDelete, index} = this.props
+    const {itemDelete, index} = this.props  
     
     itemDelete(index)
   }
+}
+
+TodoItem.propTypes = {
+  
+  test: PropTypes.string.isRequired, 
+  
+  content: PropTypes.string,  
+  itemDelete: PropTypes.func,
+  index: PropTypes.number
+}
+
+TodoItem.defaultProps = {  /*2️⃣-①：给 TodoItem 增添一些“默认属性”；*/
+  
+  test: "hello, "  /*2️⃣-②：比如增加一个默认属性 test，其值为 "hello, " 。
+                   表示，即使父组件没办法给我子组件传递这个 test 属性，我子组件依然能够
+                   使用 test 属性，因为我自己定义了一个“默认值”。
+                   */
 }
 
 export default TodoItem;
