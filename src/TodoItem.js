862ed9dbd17b6c477b92f1ed5667ed2cc4ac1940
oli-zhao <381942195@qq.com>
2019-08-09 10:15:55 +0800

    react_09

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,20 +1,26 @@
 import React, { Component } from "react";
 
 class TodoItem extends Component {
-
+  constructor(props) {
+    super(props);
+    
+    this.handleClick = this.handleClick.bind(this);
+  } 
   
   render() {
+    const {content} = this.props
+    
     return(
-      <div onClick={this.handleClick.bind(this)}>  
-        {this.props.content}
+      <div onClick={this.handleClick}> 
+        {content}  
       </div>
     )
   }
   
   handleClick() {  
-    this.props.itemDelete(this.props.index)  /*2️⃣-③：当点击列表中的某项时，
-                                             子组件调用父组件的“删除”方法，
-                                             并传入“某项”的“下标 index”。*/
+    const {itemDelete, index} = this.props
+    
+    itemDelete(index)
   }
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -7,11 +7,15 @@ import "./style.css";
 class TodoList extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       inputValue: "", 
       list: []
     };
+    
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
+    
   }
 
   render() {
@@ -25,65 +29,59 @@ class TodoList extends Component {
       
             className="input"
             value={this.state.inputValue}
-            onChange={this.handleInputChange.bind(this)}
+            onChange={this.handleInputChange}
           />
 
-          <button onClick={this.handleBtnClick.bind(this)}>
+          <button onClick={this.handleBtnClick}>
             提交
           </button>
         </div>
 
         <ul>
-          {
-            this.state.list.map((item, index)=>{ 
-              return( 
-                /*2️⃣-①：通过“属性”的形式，将 index 和 handleItemDelete 方法
-                传值给“子组件 TodoItem”；*/
-                <TodoItem 
-                  content={item}
-
-                  index={index} 
-                  itemDelete={this.handleItemDelete.bind(this)}
-                />  
-                  /*2️⃣-②：注意我在传递 handleItemDelete 方法时，我用了 .bind(this) 
-                  的写法。
-                  为什么这样写呢？
-                  答：“子组件”想要去掉其自身的某项，其实还是在借助“父组件”的方法 handleItemDelete，
-                  子组件自己是没有这个方法的！我这里将 this 的“指向”规定为“父组件 TodoList”，
-                  后边子组件调用时，this 的指向将不会出错。*/
-
-              )  
-            })
-          }
-        
+          {this.getTodoItem()}
         </ul>
 
       </Fragment>
     )
   }
 
-  handleInputChange(e) {
-    this.setState({
-      inputValue: e.target.value  
+  getTodoItem() {
+    return this.state.list.map((item, index) => { 
+      return( 
+        <TodoItem 
+          key={index}
+        
+          content={item}
+          index={index} 
+          itemDelete={this.handleItemDelete}
+        />  
+      )  
     })
+  }
+  
+  handleInputChange(e) {
+    const value = e.target.value
+    
+    this.setState(() => ({
+      inputValue: value
+    }))
   }
 
   handleBtnClick() {
-    this.setState({
-      list: [...this.state.list, this.state.inputValue],  
-      inputValue: ""  
-    }) 
+    this.setState((prevState) => ({
+      list: [...prevState.list, prevState.inputValue],  
+      inputValue: ""        
+    }))
   }
 
   handleItemDelete(index) { 
-    
-    const list = [...this.state.list] 
-    
-    list.splice(index, 1)  
-    this.setState({
-      list: list 
-    }
-    )
+    this.setState((prevState) => {
+      const list = [...prevState.list]
+      list.splice(index, 1)
+        
+      return {list}
+ 
+    })
   }
 }
 
