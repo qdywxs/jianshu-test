25722505206beef6af91c745cd9dc221a178d76a
oli-zhao <381942195@qq.com>
2019-10-07 20:21:01 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from "react";  
 
-/*2️⃣-①：引入 CSSTransition 和 TransitionGroup 这两个“组件”；*/
 import {
   CSSTransition,
   TransitionGroup,
@@ -21,9 +20,6 @@ class App extends Component  {
   
   render() {
     return (
-      /*2️⃣-②：参照官方文档例子，给要添加动画的“多个元素”的最外层包裹一个 TransitionGroup 标签；*/
-      /*2️⃣-③：给单个元素包裹 CSSTransition 标签（这个和上文讲的做法一模一样，
-      可以直接拷贝过来，❗️但要去掉属性 in）；*/
       <Fragment>
         <TransitionGroup>
           {
@@ -33,15 +29,11 @@ class App extends Component  {
                   timeout={1000}
                   classNames="fade"
                   unmountOnExit={true}
-
                   appear={true}
-
                   onEntered={(el) => {
                     el.innerHTML="hello, 前端一万小时.";
                     el.style.color="red";
-                  
                   }} 
-                  
                   key={index}
                 >
                   <div>hello, qdywxs.</div>
