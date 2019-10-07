b4f5bd5844218c5338054898984b8341a5a00522
oli-zhao <381942195@qq.com>
2019-10-07 20:14:31 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import { createStore } from "redux"; 
+import reducer from "./reducer";
 
-import reducer from "./reducer";  /*🚀从当前目录下的 reducer.js 引入 reducer。*/
-
-const store = createStore(reducer);  /*❗️❗️❗️将 reducer 作为第一个“参数”传递给“方法”createStore！*/
+const store = createStore(reducer);
 
 export default store; 
