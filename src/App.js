526f9d20f503638ab14dc6af071f009337d25e23
oli-zhao <381942195@qq.com>
2019-10-07 20:17:14 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component, Fragment } from "react";  
-
 import "./style.css";
 
 class App extends Component  {
   constructor(props) { 
     super(props);
-    
     this.state = {
       show: true  
     }
-    
     this.handleBtnToggle = this.handleBtnToggle.bind(this); 
   }
   
   render() {
     return (
       <Fragment>
-
         <div className={this.state.show ? "show" : "hide"}>hello, qdywxs.</div>
-
         <button onClick={this.handleBtnToggle}>toggle</button>
       </Fragment>
     );
